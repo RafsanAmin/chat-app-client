eff010fc715a8767b142dba9844d0fbacfb8f4e6
RafsanAmin <rafsanamin2020@gmail.com>
2022-03-09 17:47:37 +0600

    ok

---
+++ pages/num/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable import/extensions */
+import type { NextPage } from 'next';
+import React, { useState } from 'react';
+import api from '../../config/api';
+
+const B: NextPage = () => {
+  const [inp, setInp] = useState<number>(0);
+  const [ans, setAns] = useState<'Even' | 'Odd' | 'None'>();
+
+  const upDateData = async () => {
+    const resp = await fetch(`${api}api/${inp}`, {
+      method: 'POST',
+      credentials: 'include',
+    });
+    const answ = await resp.json();
+    setAns(answ.ans);
+  };
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setInp(Number(e.target.value) || 0);
+        }}
+      />
+      <h1>{`Number was ${inp}. And it is ${ans}`}</h1>
+      <button onClick={upDateData} type="button">
+        Check
+      </button>
+    </div>
+  );
+};
+
+export default B;
